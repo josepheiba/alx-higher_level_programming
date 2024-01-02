e70834f4809ba1500391597dbc410baf60cc2555
Youssef Ibba <josepheiba@icloud.com>
2024-01-02 11:45:28 -0800

    ok

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     digit = str(number)[-1]
-    print(digit, end="")
-    return(digit)
+    if (ord(digit) < 58 and ord(digit) > 47):
+        print(digit, end="")
+        return (digit)
+    return (0)
