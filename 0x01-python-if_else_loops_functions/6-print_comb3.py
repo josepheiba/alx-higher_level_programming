c6ffb6f769814c76ff7989f0f8c5e17c40038ee3
Youssef Ibba <josepheiba@icloud.com>
2024-01-02 09:14:40 -0800

    ok

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for a in range(0, 10):
     for b in range(0, 10):
-        if (a is 8 and b is 9):
+        if (a == 8 and b == 9):
             print("{}".format(str(a) + str(b)))
         elif (a < b):
             print("{}".format(str(a) + str(b)) + ", ", end="")
