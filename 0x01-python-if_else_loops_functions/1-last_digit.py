dbe57c0d3a5a7be68afee225f6386ca1b62ade59
Youssef Ibba <josepheiba@icloud.com>
2024-01-02 08:27:40 -0800

    ok

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,7 +5,7 @@ if(number < 0):
     lastDigit = int(str(number)[-1]) * -1
 else:
     lastDigit = int(str(number)[-1])
-str1 = "Last digit of "
+str1 = "Last digit of"
 str2 = "and is greater than 5"
 str3 = "and is 0"
 str4 = "and is less than 6 and not 0"
