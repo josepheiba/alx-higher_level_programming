ddad71c707dd386dfc8a914ec51a7030821cad22
Youssef Ibba <josepheiba@icloud.com>
2024-01-02 12:10:13 -0800

    ok

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-def pow(a, b):
+def fizzbuzz(a, b):
     for i in range(1, 101):
         result = ""
-        if(i % 3 == 0):
+        if (i % 3 == 0):
             result += "Fizz"
-        if(i % 5 == 0):
+        if (i % 5 == 0):
             result += "Buzz"
         print("{}".format(result) or "{}".format(i), end='')
         print(" ", end='')
