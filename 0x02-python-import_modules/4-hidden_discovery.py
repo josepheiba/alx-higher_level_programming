5c55e92543d253626647110d9663f10608f2f80a
Youssef Ibba <josepheiba@icloud.com>
2024-01-04 10:32:22 -0800

    ok

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,5 +3,5 @@ if __name__ == "__main__":
     import hidden_4 as imported
     modules = dir(imported)
     for i in range(0, len(modules)):
-        if(modules[i][0] != '_'):
+        if modules[i][0] != '_':
             print(modules[i])
