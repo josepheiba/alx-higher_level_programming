fc6671cdae05df8a9303e7661de9e51b77c959a4
Joseph Eiba <josepheiba@icloud.com>
2024-01-07 16:44:01 +0100

    ok

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,6 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for i in row:
             print("{:d}".format(i), end="")
-            if (i.index() is not (len(row) - 1)):
+            if (i is not row[len(row) - 1]):
                 print(" ", end="")
         print()
