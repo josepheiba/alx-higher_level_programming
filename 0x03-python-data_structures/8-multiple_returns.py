1064f7fbf246608832ff16a1d04f5a9173ca522b
Joseph Eiba <josepheiba@icloud.com>
2024-01-08 06:27:44 +0100

    ok

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    l = len(sentence)
-    if l == 0:
-        return (l, )
+    lenght = len(sentence)
+    if lenght == 0:
+        return (lenght, )
     else:
-        return (l, sentence[0])
+        return (lenght, sentence[0])
