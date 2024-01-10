385cae8fe2cdec55cfd6d331b8385a77223e9e3a
Joseph Eiba <josepheiba@icloud.com>
2024-01-10 19:05:18 +0100

    ok

--- 0x04-python-more_data_structures/1-main.py
+++ 0x04-python-more_data_structures/1-main.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-search_replace = __import__('1-search_replace').search_replace
+multiply_list_map = __import__('11-multiply_list_map').multiply_list_map
 
-my_list = [1, 2, 3, 4, 5, 4, 2, 1, 1, 4, 89]
-new_list = search_replace(my_list, 2, 89)
-
+my_list = [1, 2, 3, 4, 6]
+new_list = multiply_list_map(my_list, 4)
 print(new_list)
 print(my_list)
