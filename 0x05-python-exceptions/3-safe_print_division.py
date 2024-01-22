9aa04e79dc77f33005806253ccaa628ceb4e26c0
Youssef Ibba <josepheiba@icloud.com>
2024-01-22 10:32:57 -0800

    ok

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    r = None
+    try:
+        r = a / b
+        return r
+    except ZeroDivisionError:
+        return None
+    finally:
+        print(f"Inside result: {r}")
