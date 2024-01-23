9baea5b080d560279f01fa79602e837ea9a22b48
Youssef Ibba <josepheiba@icloud.com>
2024-01-23 10:50:24 -0800

    ok

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,51 @@
+class Square:
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size  # Using the setter to set the size attribute
+        self.position = position  # Using the setter to set the position attribute
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if not isinstance(value, tuple) or len(value) != 2 or not all(isinstance(i, int) and i >= 0 for i in value):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for _ in range(self.__position[1]):
+                print()
+            for _ in range(self.__size):
+                print(" " * self.__position[0] + "#" * self.__size)
+
+if __name__ == "__main__":
+    my_square = Square(4, (2, 2))
+    print("Area: {}".format(my_square.area()))
+    my_square.my_print()
+
+    print()
+
+    my_empty_square = Square()
+    print("Area: {}".format(my_empty_square.area()))
+    my_empty_square.my_print()
