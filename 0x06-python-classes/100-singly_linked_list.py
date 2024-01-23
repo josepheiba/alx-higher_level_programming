37c4616601eb4a6077285fb940c81fa234c73cfa
Youssef Ibba <josepheiba@icloud.com>
2024-01-23 11:17:51 -0800

    ok

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,55 +1,47 @@
 #!/usr/bin/python3
-"""Define a class Node."""
+"""Define a class Square."""
 
 
-class Node:
-    """Represent a node."""
-    def __init__(self, data, next_node=None):
-        self.data = data
-        self.next_node = next_node
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
 
     @property
-    def data(self):
-        return self.__data
+    def size(self):
+        return self.__size
 
-    @data.setter
-    def data(self, value):
+    @size.setter
+    def size(self, value):
         if not isinstance(value, int):
-            raise TypeError("data must be an integer")
-        self.__data = value
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
 
     @property
-    def next_node(self):
-        return self.__next_node
+    def position(self):
+        return self.__position
 
-    @next_node.setter
-    def next_node(self, value):
-        if value is not None and not isinstance(value, Node):
-            raise TypeError("next_node must be a Node object")
-        self.__next_node = value
+    @position.setter
+    def position(self, value):
+        if not isinstance(value, tuple) or len(value) != 2 or \
+           not all(isinstance(i, int) for i in value) or \
+           not all(i >= 0 for i in value):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
 
+    def area(self):
+        return self.__size ** 2
 
-class SinglyLinkedList:
-    def __init__(self):
-        self.head = None
-
-    def sorted_insert(self, value):
-        new_node = Node(value)
-        if self.head is None or value < self.head.data:
-            new_node.next_node = self.head
-            self.head = new_node
+    def my_print(self):
+        if self.__size == 0:
+            print()
         else:
-            current = self.head
-            while (current.next_node is not None and
-                current.next_node.data < value):
-                current = current.next_node
-            new_node.next_node = current.next_node
-            current.next_node = new_node
-
-    def __str__(self):
-        result = ""
-        current = self.head
-        while current is not None:
-            result += str(current.data) + "\n"
-            current = current.next_node
-        return result
+            for _ in range(self.__position[1]):
+                print()
+            for _ in range(self.__size):
+                print(" " * self.__position[0], end="")
+                print("#" * self.__size)
---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,14 @@
+import math
+
+class MagicClass:
+    def __init__(self, radius):
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError('radius must be a number')
+
+        self.__radius = radius
+
+    def area(self):
+        return 2 ** self.__radius * math.pi
+
+    def circumference(self):
+        return 2 * math.pi * self.__radius
