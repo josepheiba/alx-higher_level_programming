44b02fb369b8236da2eb34407861e9f23c6074c2
Joseph Eiba <josepheiba@icloud.com>
2024-01-29 17:46:09 +0100

    ok

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""docs"""
+
+
+class Rectangle:
+    """docs"""
+    def __init__(self, width=0, height=0):
+        self._width = width
+        self._height = height
+
+    @property
+    def width(self):
+        return self._width
+
+    @width.setter
+    def width(self, value):
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        self._width = value
+
+    @property
+    def height(self):
+        return self._height
+
+    @height.setter
+    def height(self, value):
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        self._height = value
