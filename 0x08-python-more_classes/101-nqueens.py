d6b50d396694253d31341941c8cd0b6ffa0ba8c9
Joseph Eiba <josepheiba@icloud.com>
2024-01-30 00:08:49 +0100

    ok

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -31,16 +31,7 @@ def get_solution(board):
 
 
 def xout(board, row, col):
-    """X out spots on a chessboard.
-
-    All spots where non-attacking queens can no
-    longer be played are X-ed out.
-
-    Args:
-        board (list): The current working chessboard.
-        row (int): The row where a queen was last played.
-        col (int): The column where a queen was last played.
-    """
+    """docs"""
     # X out all forward spots
     for c in range(col + 1, len(board)):
         board[row][c] = "x"
@@ -84,16 +75,7 @@ def xout(board, row, col):
 
 
 def recursive_solve(board, row, queens, solutions):
-    """Recursively solve an N-queens puzzle.
-
-    Args:
-        board (list): The current working chessboard.
-        row (int): The current working row.
-        queens (int): The current number of placed queens.
-        solutions (list): A list of lists of solutions.
-    Returns:
-        solutions
-    """
+    """docs"""
     if queens == len(board):
         solutions.append(get_solution(board))
         return (solutions)
