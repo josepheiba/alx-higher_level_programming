fdade35f7c401ff4b67fe9248f377496af2b4db1
Joseph Eiba <josepheiba@icloud.com>
2024-01-29 19:54:31 +0100

    ok

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -41,7 +41,7 @@ class Rectangle:
         return 2 * (self.__height + self.__width)
 
     def __str__(self):
-        drawing = ""
+        drawing = ''
         if self.perimeter == 0:
             return drawing
         for _ in range(self.__height):
