56f96de007df12571b49ac3bfb3942d76185be8c
Joseph Eiba <josepheiba@icloud.com>
2024-01-30 14:56:16 +0100

    ok

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""docs"""
+def magic_string():
+    School()
+    return ", ".join(["BestSchool"] * School.number_of_instances)
+
+
+class School:
+    """docs"""
+    number_of_instances = 0
+
+    def __init__(self):
+        School.number_of_instances += 1
