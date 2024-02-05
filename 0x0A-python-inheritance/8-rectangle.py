47035520487ec1d2237e66a9c772c3c4ec6e2a6b
Joseph Eiba <josepheiba@icloud.com>
2024-02-05 23:03:26 +0100

    auto_commit

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""docs"""
+
+
+class BaseGeometry:
+    """docs"""
+
+    def area(self):
+        """docs"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """docs"""
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+
+class Rectangle(BaseGeometry):
+    """docs"""
+
+    def __init__(self, width, height):
+        """docs"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
