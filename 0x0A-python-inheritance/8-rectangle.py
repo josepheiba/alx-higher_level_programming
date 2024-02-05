de6eca5bd13e470bbb79a07d51bce80061bb6baa
Joseph Eiba <josepheiba@icloud.com>
2024-02-05 23:24:39 +0100

    auto_commit

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -2,36 +2,14 @@
 """docs"""
 
 
-class BaseGeometry:
-    """docs"""
-
-    def area(self):
-        """docs"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """docs"""
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """Represents a rectangle.
-    Private instance attributes:
-        - width
-        - height
-    Inherits from BaseGeometry.
-    """
+    """docs"""
 
     def __init__(self, width, height):
-        """Initializes an instance.
-        Args:
-            - width: width of the rectangle
-            - heigth: height of the rectangle
-        """
-
+        """docs"""
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
