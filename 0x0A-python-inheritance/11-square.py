6d908122e7b1670a7e6d35b37ef4933c692d2533
Joseph Eiba <josepheiba@icloud.com>
2024-02-05 23:51:12 +0100

    auto_commit

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""docs"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """docs"""
+
+    def __init__(self, size):
+        """docs"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """docs"""
+        return super().area()
+
+    def __str__(sef):
+        """docs"""
+        return super().__str__()
