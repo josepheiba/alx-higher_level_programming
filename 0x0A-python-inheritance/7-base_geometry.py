47e122c16f3c42658bec397de7f6a83c3b847f38
Joseph Eiba <josepheiba@icloud.com>
2024-02-05 22:49:49 +0100

    auto_commit

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -6,9 +6,11 @@ class BaseGeometry:
     """docs"""
 
     def area(self):
+        """docs"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
+        """docs"""
         if not isinstance(value, int):
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
