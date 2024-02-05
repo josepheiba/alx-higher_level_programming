96e23b3c8a8c9efc743d70ca1bad74960e86be3b
Joseph Eiba <josepheiba@icloud.com>
2024-02-05 23:07:49 +0100

    auto_commit

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,20 +1,6 @@
 #!/usr/bin/python3
 """docs"""
-
-
-class BaseGeometry:
-    """docs"""
-
-    def area(self):
-        """docs"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """docs"""
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+BaseGeometry = __import__('7-base-geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
