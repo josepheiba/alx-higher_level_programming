36bc834cfa4119aaff1ea5a12ad297dbcc0c76cf
Joseph Eiba <josepheiba@icloud.com>
2024-02-06 19:06:46 +0100

    auto_commit

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""docs"""
+
+
+def write_file(filename="", text=""):
+    """docs"""
+    with open(filename, mode='w', encoding='utf-8') as fileObject:
+        fileObject.write(text)
