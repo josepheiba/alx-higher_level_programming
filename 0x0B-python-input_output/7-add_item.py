7d791c287d902da2cbddf0b0ba8e8266f29b9ab1
Joseph Eiba <josepheiba@icloud.com>
2024-02-06 23:27:05 +0100

    auto_commit

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,7 +6,6 @@ import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-
 fileName = 'add_item.json'
 try:
     myList = load_from_json_file(fileName)
