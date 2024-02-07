3d52960126488e54a8b0ef8418931093a13b73f6
Joseph Eiba <josepheiba@icloud.com>
2024-02-07 01:29:17 +0100

    auto_commit

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -7,12 +7,12 @@ def pascal_triangle(n):
     if n <= 0:
         return []
 
-    triangles = [[1]]
-    while len(triangles) != n:
-        tri = triangles[-1]
+    tris = [[1]]
+    while len(tris) != n:
+        tri = tris[-1]
         tmp = [1]
         for i in range(len(tri) - 1):
             tmp.append(tri[i] + tri[i + 1])
         tmp.append(1)
-        triangles.append(tmp)
-    return triangles
+        tris.append(tmp)
+    return tris
