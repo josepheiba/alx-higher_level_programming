1231ac00ccd9cc3e889c59b2fd852837e96fd00e
Joseph Eiba <josepheiba@icloud.com>
2024-02-07 01:07:09 +0100

    auto_commit

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""docs"""
+
+
+class Student:
+    """docs"""
+
+    def __init__(self, first_name, last_name, age):
+        """docs"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """docs"""
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """docs"""
+        for a, b in json.items():
+            setattr(self, a, b)
