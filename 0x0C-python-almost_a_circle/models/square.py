adcb86470c263d9633e5e797e9462fba6296336c
Joseph Eiba <josepheiba@icloud.com>
2024-02-12 22:24:39 +0100

    auto_commit

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -12,6 +12,7 @@ class Square(Rectangle):
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
+        """docs"""
         return "[Square] ({}) {}/{} - {}".format(
         self.id, self.x, self.y, self.width)
 
