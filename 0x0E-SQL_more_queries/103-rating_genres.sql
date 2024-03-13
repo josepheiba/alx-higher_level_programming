57b5a3a06e969f343579a46806294f4ee72a73a9
Joseph Eiba <josepheiba@icloud.com>
2024-03-13 21:47:28 +0000

    ok

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,8 +1,7 @@
 -- show grants
-SELECT tv_genres.name, SUM(hbtn_0d_tvshows_rate.rate) AS rating_sum
-FROM tv_genres
-JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-JOIN hbtn_0d_tvshows_rate ON tv_shows.id = hbtn_0d_tvshows_rate.show_id
-GROUP BY tv_genres.name
-ORDER BY rating_sum DESC;
+SELECT tg.name, SUM(tr.rate) AS rating 
+FROM tv_show_genres tgs
+JOIN tv_genres tg ON tg.id = tgs.genre_id
+JOIN tv_show_ratings tr ON tr.show_id = tgs.show_id
+GROUP BY tg.name
+ORDER BY rating DESC;
