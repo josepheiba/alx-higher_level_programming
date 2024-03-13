349cfb9542da540414288295f0789a2fb357b36d
Joseph Eiba <josepheiba@icloud.com>
2024-03-13 21:26:10 +0000

    ok

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
 -- show grants
-SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_shows.title, tv_show_genres.genre_id;
+SELECT cities.id, cities.name, states.name FROM cities
+INNER JOIN states ON cities.state_id = states.id
+ORDER BY cities.id;
