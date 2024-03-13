a8320b8f8c3b113d7fddc80ef6233021869c3091
Joseph Eiba <josepheiba@icloud.com>
2024-03-13 21:45:42 +0000

    ok

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,6 +1,6 @@
 -- show grants
-SELECT tv_shows.title, SUM(hbtn_0d_tvshows_rate.rating) AS rating_sum
-FROM tv_shows
-JOIN hbtn_0d_tvshows_rate ON tv_shows.id = hbtn_0d_tvshows_rate.show_id
-GROUP BY tv_shows.title
-ORDER BY rating_sum DESC;
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating 
+FROM tv_shows, tv_show_ratings 
+WHERE tv_shows.id = tv_show_ratings.show_id 
+GROUP BY tv_shows.title 
+ORDER BY rating DESC;
